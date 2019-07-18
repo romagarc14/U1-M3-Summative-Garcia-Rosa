b4b25b3d3a98c14aabd6329dc4a88d0a15d47cd0
Rosa G <51469712+romagarc14@users.noreply.github.com>
2019-07-18 14:11:43 -0500

    first commit

---
+++ src/main/java/com/company/U1M3SummativeGarciaRosa/U1M3SummativeGarciaRosaApplication.java
@@ -0,0 +1,13 @@
+package com.company.U1M3SummativeGarciaRosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U1M3SummativeGarciaRosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U1M3SummativeGarciaRosaApplication.class, args);
+	}
+
+}
